1bb51b2de57e77b2f32c88974aade7d8f4544333
omarFarukGit <muhammadabuhafz@gmail.com>
2026-05-20 08:00:01 +0600

    Enhance AddRoomPage with amenities selection using checkboxes and update form submission logic

--- src/app/add-room/page.jsx
+++ src/app/add-room/page.jsx
@@ -1,40 +1,61 @@
 "use client";
 
-import { RadioSection } from "@/components/heroUI/RadioButton";
+import { useState } from "react";
 import { authClient } from "@/lib/auth-client";
 import {
   FieldError,
   Input,
   Label,
   TextField,
-  Select,
-  ListBox,
   TextArea,
   Button,
   Card,
-  Description,
-  RadioGroup,
+  Checkbox,
 } from "@heroui/react";
-import { Radio } from "lucide-react";
+
+const amenitiesList = [
+  "Whiteboard",
+  "Projector",
+  "Wi-Fi",
+  "Air Conditioning",
+  "Quiet Zone",
+  "Power Outlets",
+];
 
 const AddRoomPage = () => {
   const { data: session } = authClient.useSession();
+
   const ownerName = session?.user?.name;
   const ownerEmail = session?.user?.email;
   const userId = session?.user?.id;
 
-  console.log(session);
+  const [amenities, setAmenities] = useState([]);
+
+  const handleAmenities = (value, checked) => {
+    if (checked) {
+      setAmenities((prev) => [...prev, value]);
+    } else {
+      setAmenities((prev) => prev.filter((item) => item !== value));
+    }
+  };
+  console.log(amenities);
+
   const onSubmit = async (e) => {
     e.preventDefault();
+
     const formData = new FormData(e.currentTarget);
+
     const room = Object.fromEntries(formData.entries());
 
     const roomData = {
       userId,
       ownerName,
       ownerEmail,
+      amenities,
       ...room,
     };
+
+    console.log(roomData);
 
     const res = await fetch(`http://localhost:3001/api/study-nook`, {
       method: "POST",
@@ -45,80 +66,96 @@ const AddRoomPage = () => {
     });
 
     const data = await res.json();
+
+    console.log(data);
   };
 
   return (
     <div className="p-5 max-w-7xl mx-auto bg-blue-950">
       <h1 className="text-2xl font-bold">Add Room</h1>
-      <p>
-        Share your study room with others. You can edit or remove it any time.
-      </p>
 
       <Card>
         <form onSubmit={onSubmit} className="p-10 space-y-8 w-3xl">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-            {/* Destination Name */}
             <div className="md:col-span-2">
               <TextField name="roomName" isRequired>
                 <Label>Room Name</Label>
-                <Input placeholder="" className="rounded-2xl" />
+                <Input className="rounded-2xl" />
                 <FieldError />
               </TextField>
             </div>
-            {/* Description */}
+
             <div className="md:col-span-2">
               <TextField name="description" isRequired>
                 <Label>Description</Label>
-                <TextArea
-                  placeholder="Describe the travel experience..."
-                  className="rounded-3xl"
-                />
+
+                <TextArea className="rounded-3xl" />
+
                 <FieldError />
               </TextField>
             </div>
-            {/* Image URL - Removed preview */}
+
             <div className="md:col-span-2">
               <TextField name="image" isRequired>
                 <Label>Image URL</Label>
-                <Input
-                  type="url"
-                  placeholder="https://example.com/bali-paradise.jpg"
-                  className="rounded-2xl"
-                />
+
+                <Input type="url" className="rounded-2xl" />
+
                 <FieldError />
               </TextField>
             </div>
 
-            {/* Price */}
             <TextField name="floor" type="number" isRequired>
-              <Label>Floor </Label>
-              <Input type="number" placeholder="1299" className="rounded-2xl" />
+              <Label>Floor</Label>
+
+              <Input type="number" className="rounded-2xl" />
+
               <FieldError />
             </TextField>
-            <TextField name="cpacity" type="number" isRequired>
-              <Label>Cpacity </Label>
-              <Input
-                type="number"
-                placeholder="e.g floor 3rd"
-                className="rounded-2xl"
-              />
+
+            <TextField name="capacity" type="number" isRequired>
+              <Label>Capacity</Label>
+
+              <Input type="number" className="rounded-2xl" />
+
               <FieldError />
             </TextField>
+
             <TextField name="hourlyRate" type="number" isRequired>
-              <Label>Hourly Rate </Label>
-              <Input type="number" placeholder="1299" className="rounded-2xl" />
+              <Label>Hourly Rate</Label>
+
+              <Input type="number" className="rounded-2xl" />
+
               <FieldError />
             </TextField>
           </div>
-          <div>
-            <RadioSection />
+
+          {/* Amenities */}
+
+          <div className="flex flex-col gap-4">
+            <Label>Amenities</Label>
+
+            <div className="grid grid-cols-3 gap-2">
+              {amenitiesList.map((item) => (
+                <Checkbox
+                  key={item}
+                  onChange={(checked) => handleAmenities(item, checked)}
+                >
+                  <Checkbox.Control>
+                    <Checkbox.Indicator />
+                  </Checkbox.Control>
+
+                  <Checkbox.Content>
+                    <Label>{item}</Label>
+                  </Checkbox.Content>
+                </Checkbox>
+              ))}
+            </div>
           </div>
-          {/* Buttons */}
 
           <Button
             type="submit"
-            variant="outline"
-            className=" rounded-none w-full bg-cyan-500 text-white"
+            className="rounded-none w-full bg-cyan-500 text-white"
           >
             Add Room
           </Button>
